b87b9de13a82512b72fd63901f0757c9b11a000c
Filip Gabrys <el.filberto@gmail.com>
2022-07-13 11:47:11 +0200

    Add init files

---
+++ src/test/java/com/imdbmovie/kodillafinalproject/KodillaFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imdbmovie.kodillafinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodillaFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
